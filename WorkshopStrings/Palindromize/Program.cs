6a13d463167a923abe7dc731f12c47fb55cd1e66
SimoPrG <simeon_petev@hotmail.com>
2015-11-29 17:41:24 +0200

    Strings and Greedy

---
+++ WorkshopStrings/Palindromize/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Palindromize
+{
+    using System;
+    using System.Text;
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            string str = Console.ReadLine();
+
+            var palindromize = Palindromize(str);
+            var answer = palindromize;
+
+            Console.WriteLine(answer);
+        }
+
+        private static bool IsPalindrome(StringBuilder str)
+        {
+            for (int i = 0; i < str.Length / 2; i++)
+            {
+                if (str[i] != str[str.Length - i - 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static string Palindromize(string str)
+        {
+            var builder = new StringBuilder(str);
+            int thePosition = builder.Length;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (IsPalindrome(builder))
+                {
+                    return builder.ToString();
+                }
+
+                builder.Insert(thePosition, builder[i]);
+            }
+
+            return "ne6to se obyrka";
+        }
+    }
+}
---
+++ WorkshopStrings/Rabbits/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Rabbis
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var answers = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int n = answers.Length - 1;
+            var dict = new Dictionary<int, double>();
+            for (int i = 0; i < n; i++)
+            {
+                int ansewer = answers[i] + 1;
+                if (dict.ContainsKey(ansewer))
+                {
+                    dict[ansewer]++;
+                }
+                else
+                {
+                    dict.Add(ansewer, 1);
+                }
+            }
+
+            double answer = 0;
+            foreach (var kvp in dict)
+            {
+                answer += kvp.Key * Math.Ceiling(kvp.Value / kvp.Key);
+            }
+
+            Console.WriteLine(answer);
+        }
+    }
+}
